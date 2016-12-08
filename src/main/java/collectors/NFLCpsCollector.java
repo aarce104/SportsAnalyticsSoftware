92119c95c1cb435d1c15412405eece4df0ea6c79
aarce <aarce@udel.edu>
2016-12-07 23:44:05 -0500

    new update

--- src/main/java/collectors/NFLCpsCollector.java
+++ src/main/java/collectors/NFLCpsCollector.java
@@ -14,11 +14,11 @@ public class NFLCpsCollector implements CollectorGeneric {
     private final static String PUNTER = "P";
     private final static String QUARTERBACK = "QB";
     private final static String KICKER = "K";
-    private final static String RUNNINGBACK = "RB";
+    private final static String RUNNING_BACK = "RB";
     private final static String WIDE_RECEIVER = "WR";
-    private final static String TIGHTEND = "TE";
+    private final static String TIGHT_END = "TE";
     private final static String FULLBACK = "FB";
-    private final static String LONGSNAPPER = "LS";
+    private final static String LONG_SNAPPER = "LS";
     private final static String POSITION = "Position";
     private final static String ID = "ID";
     private final static String TEAM = "team";
@@ -73,7 +73,7 @@ public class NFLCpsCollector implements CollectorGeneric {
     private final static String INT_YDS = "IntYds";
     private final static String INT_AVERAGE = "IntAverage";
     private final static String INT_LNG = "IntLng";
-    private final static String PASSED_DEFENDED = "PassesDefended";
+    private final static String PASSES_DEFENDED = "PassesDefended";
 
     private final static String FUMBLES = "Fumbles";
     private final static String FUM_LOST = "FumLost";
@@ -138,7 +138,7 @@ public class NFLCpsCollector implements CollectorGeneric {
     private final static String STUFFS = "Stuffs";
     private final static String STUFF_YDS = "StuffYds";
 
-    private final static String KB = "KB";
+    private final static String BLOCKED_KICKS = "KB";
 
     private final static String FG_BLK = "FgBlk";
     private final static String FG_MADE = "FgMade";
@@ -149,7 +149,7 @@ public class NFLCpsCollector implements CollectorGeneric {
     private final static String FG_1_19_PCT = "Fg1_19Pct";
     private final static String FG_MADE_20_29 = "FgMade20_29";
     private final static String FG_ATT_20_29 = "FgAtt20_29";
-    private final static String FG_20_29 = "Fg20_29Pct";
+    private final static String FG_20_29_PCT = "Fg20_29Pct";
     private final static String FG_MADE_30_39 = "FgMade30_39";
     private final static String FG_ATT_30_39 = "FgAtt30_39";
     private final static String FG_30_39_PCT = "Fg30_39Pct";
@@ -176,8 +176,6 @@ public class NFLCpsCollector implements CollectorGeneric {
 
 
 
-
-
     public NFLCpsCollector() {
 
     }
@@ -198,13 +196,163 @@ public class NFLCpsCollector implements CollectorGeneric {
     public void enrichPlayer(JsonObject player) {
         JsonObject playerJson = new JsonObject();
         String playerPosition = player.getAsJsonObject(PLAYER).get(POSITION).getAsString();
+
         int GP = player.getAsJsonObject(STATS).getAsJsonObject(GAMES_PLAYED).get("#text").getAsInt();
+
         int PassAtt = player.getAsJsonObject(STATS).getAsJsonObject(PASS_ATTEMPTS).get("#text").getAsInt();
+        int PassCMP = player.getAsJsonObject(STATS).getAsJsonObject(PASS_COMPLETIONS).get("#text").getAsInt();
+        int PassPCT = player.getAsJsonObject(STATS).getAsJsonObject(PASS_PCT).get("#text").getAsInt();
+        int PassYds = player.getAsJsonObject(STATS).getAsJsonObject(PASS_YDS).get("#text").getAsInt();
+        int PassAvg = player.getAsJsonObject(STATS).getAsJsonObject(PASS_AVG).get("#text").getAsInt();
+        int PassYdsPerAtt = player.getAsJsonObject(STATS).getAsJsonObject(PASS_YARDS_PER_ATT).get("#text").getAsInt();
+        int PassTD = player.getAsJsonObject(STATS).getAsJsonObject(PASS_TD).get("#text").getAsInt();
+        int PassTDPct = player.getAsJsonObject(STATS).getAsJsonObject(PASS_TD_PCT).get("#text").getAsInt();
+        int PassInt = player.getAsJsonObject(STATS).getAsJsonObject(PASS_INT).get("#text").getAsInt();
+        int PassIntPct = player.getAsJsonObject(STATS).getAsJsonObject(PASS_INT_PCT).get("#text").getAsInt();
+        int PassLng = player.getAsJsonObject(STATS).getAsJsonObject(PASS_LNG).get("#text").getAsInt();
+        int Pass20Plus = player.getAsJsonObject(STATS).getAsJsonObject(PASS_20_PLUS).get("#text").getAsInt();
+        int Pass40Plus = player.getAsJsonObject(STATS).getAsJsonObject(PASS_40_PLUS).get("#text").getAsInt();
+        int PassSacks = player.getAsJsonObject(STATS).getAsJsonObject(PASS_SACKS).get("#text").getAsInt();
+        int PassSacksY = player.getAsJsonObject(STATS).getAsJsonObject(PASS_SACK_Y).get("#text").getAsInt();
+        int QBRating = player.getAsJsonObject(STATS).getAsJsonObject(QBR_RATING).get("#text").getAsInt();
+
+        int Targets = player.getAsJsonObject(STATS).getAsJsonObject(TARGETS).get("#text").getAsInt();
+        int Receptions = player.getAsJsonObject(STATS).getAsJsonObject(RECEPTIONS).get("#text").getAsInt();
+        int RecYds = player.getAsJsonObject(STATS).getAsJsonObject(REC_YARDS).get("#text").getAsInt();
+        int RecAvg = player.getAsJsonObject(STATS).getAsJsonObject(REC_AVERAGE).get("#text").getAsInt();
+        int RecTD = player.getAsJsonObject(STATS).getAsJsonObject(REC_TD).get("#text").getAsInt();
+        int RecLng = player.getAsJsonObject(STATS).getAsJsonObject(REC_LNG).get("#text").getAsInt();
+        int Rec20Plus = player.getAsJsonObject(STATS).getAsJsonObject(REC_20_PLUS).get("#text").getAsInt();
+        int Rec40Plus = player.getAsJsonObject(STATS).getAsJsonObject(REC_40_PLUS).get("#text").getAsInt();
+        int RecFumbles = player.getAsJsonObject(STATS).getAsJsonObject(REC_FUMBLES).get("#text").getAsInt();
+
+        int RushAtt = player.getAsJsonObject(STATS).getAsJsonObject(RUSH_ATTEMPTS).get("#text").getAsInt();
+        int RushYds = player.getAsJsonObject(STATS).getAsJsonObject(RUSH_YARDS).get("#text").getAsInt();
+        int RushAvg = player.getAsJsonObject(STATS).getAsJsonObject(RUSH_AVERAGE).get("#text").getAsInt();
+        int RushTD = player.getAsJsonObject(STATS).getAsJsonObject(RUSH_TD).get("#text").getAsInt();
+        int RushLng = player.getAsJsonObject(STATS).getAsJsonObject(RUSH_LNG).get("#text").getAsInt();
+        int Rush20Plus = player.getAsJsonObject(STATS).getAsJsonObject(RUSH_20_PLUS).get("#text").getAsInt();
+        int Rush40Plus = player.getAsJsonObject(STATS).getAsJsonObject(RUSH_40_PLUS).get("#text").getAsInt();
+        int RushFumbles = player.getAsJsonObject(STATS).getAsJsonObject(RUSH_FUMBLES).get("#text").getAsInt();
+
+        int TackleSolo = player.getAsJsonObject(STATS).getAsJsonObject(TACKLE_SOLO).get("#text").getAsInt();
+        int TackleTotal = player.getAsJsonObject(STATS).getAsJsonObject(TACKLE_TOTAL).get("#text").getAsInt();
+        int TackleAst = player.getAsJsonObject(STATS).getAsJsonObject(TACKLE_AST).get("#text").getAsInt();
+        int Sacks = player.getAsJsonObject(STATS).getAsJsonObject(SACKS).get("#text").getAsInt();
+        int SackYds = player.getAsJsonObject(STATS).getAsJsonObject(SACK_YDS).get("#text").getAsInt();
+        int Safeties = player.getAsJsonObject(STATS).getAsJsonObject(SAFETIES).get("#text").getAsInt();
+
+        int Interceptions = player.getAsJsonObject(STATS).getAsJsonObject(INTERCEPTIONS).get("#text").getAsInt();
+        int IntTD = player.getAsJsonObject(STATS).getAsJsonObject(INT_TD).get("#text").getAsInt();
+        int IntYds = player.getAsJsonObject(STATS).getAsJsonObject(INT_YDS).get("#text").getAsInt();
+        int IntAvg = player.getAsJsonObject(STATS).getAsJsonObject(INT_AVERAGE).get("#text").getAsInt();
+        int IntLng = player.getAsJsonObject(STATS).getAsJsonObject(INT_LNG).get("#text").getAsInt();
+        int PassesDefended = player.getAsJsonObject(STATS).getAsJsonObject(PASSES_DEFENDED).get("#text").getAsInt();
+
+        int Fumbles = player.getAsJsonObject(STATS).getAsJsonObject(FUMBLES).get("#text").getAsInt();
+        int FumLost = player.getAsJsonObject(STATS).getAsJsonObject(FUM_LOST).get("#text").getAsInt();
+        int FumForced = player.getAsJsonObject(STATS).getAsJsonObject(FUM_FORCED).get("#text").getAsInt();
+        int FumOwnRec = player.getAsJsonObject(STATS).getAsJsonObject(FUM_OWN_REC).get("#text").getAsInt();
+        int FumOppRec = player.getAsJsonObject(STATS).getAsJsonObject(FUM_OPP_REC).get("#text").getAsInt();
+        int FumRecYds = player.getAsJsonObject(STATS).getAsJsonObject(FUM_REC_YDS).get("#text").getAsInt();
+        int FumTotalRec = player.getAsJsonObject(STATS).getAsJsonObject(FUM_TOTAL_REC).get("#text").getAsInt();
+        int FumTD = player.getAsJsonObject(STATS).getAsJsonObject(FUM_TD).get("#text").getAsInt();
+
+        int KrRet = player.getAsJsonObject(STATS).getAsJsonObject(KR_RET).get("#text").getAsInt();
+        int KrYds = player.getAsJsonObject(STATS).getAsJsonObject(KR_YDS).get("#text").getAsInt();
+        int KrAvg = player.getAsJsonObject(STATS).getAsJsonObject(KR_AVG).get("#text").getAsInt();
+        int KrLng = player.getAsJsonObject(STATS).getAsJsonObject(KR_LNG).get("#text").getAsInt();
+        int KrTD = player.getAsJsonObject(STATS).getAsJsonObject(KR_TD).get("#text").getAsInt();
+        int Kr20Plus = player.getAsJsonObject(STATS).getAsJsonObject(KR_20_PLUS).get("#text").getAsInt();
+        int Kr40Plus = player.getAsJsonObject(STATS).getAsJsonObject(KR_40_PLUS).get("#text").getAsInt();
+        int KrFC = player.getAsJsonObject(STATS).getAsJsonObject(KR_FC).get("#text").getAsInt();
+        int KrFum = player.getAsJsonObject(STATS).getAsJsonObject(KR_FUM).get("#text").getAsInt();
+
+        int Kickoffs = player.getAsJsonObject(STATS).getAsJsonObject(KICKOFFS).get("#text").getAsInt();
+        int KoYds = player.getAsJsonObject(STATS).getAsJsonObject(KO_YDS).get("#text").getAsInt();
+        int KoOOB = player.getAsJsonObject(STATS).getAsJsonObject(KO_OOB).get("#text").getAsInt();
+        int KoAvg = player.getAsJsonObject(STATS).getAsJsonObject(KO_AVG).get("#text").getAsInt();
+        int KoTB = player.getAsJsonObject(STATS).getAsJsonObject(KO_TB).get("#text").getAsInt();
+        int KoPct = player.getAsJsonObject(STATS).getAsJsonObject(KO_PCT).get("#text").getAsInt();
+        int KoRet = player.getAsJsonObject(STATS).getAsJsonObject(KO_RET).get("#text").getAsInt();
+        int KoRetYds = player.getAsJsonObject(STATS).getAsJsonObject(KO_RET_YDS).get("#text").getAsInt();
+        int KoRetAvgYds = player.getAsJsonObject(STATS).getAsJsonObject(KO_RET_AVG_YDS).get("#text").getAsInt();
+        int KoTD = player.getAsJsonObject(STATS).getAsJsonObject(KO_TD).get("#text").getAsInt();
+        int KoOS = player.getAsJsonObject(STATS).getAsJsonObject(KO_OS).get("#text").getAsInt();
+        int KoOSR = player.getAsJsonObject(STATS).getAsJsonObject(KO_OSR).get("#text").getAsInt();
+
+        int PrRet = player.getAsJsonObject(STATS).getAsJsonObject(PR_RET).get("#text").getAsInt();
+        int PrYds = player.getAsJsonObject(STATS).getAsJsonObject(PR_YDS).get("#text").getAsInt();
+        int PrAvg = player.getAsJsonObject(STATS).getAsJsonObject(PR_AVG).get("#text").getAsInt();
+        int PrLng = player.getAsJsonObject(STATS).getAsJsonObject(PR_LNG).get("#text").getAsInt();
+        int PrTD = player.getAsJsonObject(STATS).getAsJsonObject(PR_TD).get("#text").getAsInt();
+        int Pr20Plus = player.getAsJsonObject(STATS).getAsJsonObject(PR_20_PLUS).get("#text").getAsInt();
+        int Pr40Plus = player.getAsJsonObject(STATS).getAsJsonObject(PR_40_PLUS).get("#text").getAsInt();
+        int PrFC = player.getAsJsonObject(STATS).getAsJsonObject(PR_FC).get("#text").getAsInt();
+        int PrFum = player.getAsJsonObject(STATS).getAsJsonObject(PR_FUM).get("#text").getAsInt();
+
+        int Punts = player.getAsJsonObject(STATS).getAsJsonObject(PUNTS).get("#text").getAsInt();
+        int PuntYds = player.getAsJsonObject(STATS).getAsJsonObject(PUNT_YDS).get("#text").getAsInt();
+        int PuntNetYds = player.getAsJsonObject(STATS).getAsJsonObject(PUNT_NET_YDS).get("#text").getAsInt();
+        int PuntLng = player.getAsJsonObject(STATS).getAsJsonObject(PUNT_LNG).get("#text").getAsInt();
+        int PuntAvg = player.getAsJsonObject(STATS).getAsJsonObject(PUNT_AVG).get("#text").getAsInt();
+        int PuntNetAvg = player.getAsJsonObject(STATS).getAsJsonObject(PUNT_NET_AVG).get("#text").getAsInt();
+        int PuntBlk = player.getAsJsonObject(STATS).getAsJsonObject(PUNT_BLK).get("#text").getAsInt();
+        int PuntOOB = player.getAsJsonObject(STATS).getAsJsonObject(PUNT_OOB).get("#text").getAsInt();
+        int PuntDown = player.getAsJsonObject(STATS).getAsJsonObject(PUNT_DOWN).get("#text").getAsInt();
+        int PuntIn20 = player.getAsJsonObject(STATS).getAsJsonObject(PUNT_IN_20).get("#text").getAsInt();
+        int PuntIn20Pct = player.getAsJsonObject(STATS).getAsJsonObject(PUNT_IN_20_PCT).get("#text").getAsInt();
+        int PuntTB = player.getAsJsonObject(STATS).getAsJsonObject(PUNT_TB).get("#text").getAsInt();
+        int PuntTBPct = player.getAsJsonObject(STATS).getAsJsonObject(PUNT_TB_PCT).get("#text").getAsInt();
+        int PuntFC = player.getAsJsonObject(STATS).getAsJsonObject(PUNT_FC).get("#text").getAsInt();
+        int PuntRet = player.getAsJsonObject(STATS).getAsJsonObject(PUNT_RET).get("#text").getAsInt();
+        int PuntRetYds = player.getAsJsonObject(STATS).getAsJsonObject(PUNT_RET_YDS).get("#text").getAsInt();
+        int PuntRetAvg = player.getAsJsonObject(STATS).getAsJsonObject(PUNT_RET_AVG).get("#text").getAsInt();
+
+        int Stuffs = player.getAsJsonObject(STATS).getAsJsonObject(STUFFS).get("#text").getAsInt();
+        int StuffYds = player.getAsJsonObject(STATS).getAsJsonObject(STUFF_YDS).get("#text").getAsInt();
+
+        int KB = player.getAsJsonObject(STATS).getAsJsonObject(BLOCKED_KICKS).get("#text").getAsInt();
+
+        int FgBlk = player.getAsJsonObject(STATS).getAsJsonObject(FG_BLK).get("#text").getAsInt();
+        int FgMade = player.getAsJsonObject(STATS).getAsJsonObject(FG_MADE).get("#text").getAsInt();
+        int FgAtt = player.getAsJsonObject(STATS).getAsJsonObject(FG_ATT).get("#text").getAsInt();
+        int FgPct = player.getAsJsonObject(STATS).getAsJsonObject(FG_PCT).get("#text").getAsInt();
+        int FgMade1_19 = player.getAsJsonObject(STATS).getAsJsonObject(FG_MADE_1_19).get("#text").getAsInt();
+        int FgAtt1_19 = player.getAsJsonObject(STATS).getAsJsonObject(FG_ATT_1_19).get("#text").getAsInt();
+        int Fg1_19Pct = player.getAsJsonObject(STATS).getAsJsonObject(FG_1_19_PCT).get("#text").getAsInt();
+        int FgMade20_29 = player.getAsJsonObject(STATS).getAsJsonObject(FG_MADE_20_29).get("#text").getAsInt();
+        int FgAtt20_29 = player.getAsJsonObject(STATS).getAsJsonObject(FG_ATT_20_29).get("#text").getAsInt();
+        int Fg20_29Pct = player.getAsJsonObject(STATS).getAsJsonObject(FG_20_29_PCT).get("#text").getAsInt();
+        int FgMade30_39 = player.getAsJsonObject(STATS).getAsJsonObject(FG_MADE_30_39).get("#text").getAsInt();
+        int FgAtt30_39 = player.getAsJsonObject(STATS).getAsJsonObject(FG_ATT_30_39).get("#text").getAsInt();
+        int Fg30_39Pct = player.getAsJsonObject(STATS).getAsJsonObject(FG_30_39_PCT).get("#text").getAsInt();
+        int FgMade40_49 = player.getAsJsonObject(STATS).getAsJsonObject(FG_MADE_40_49).get("#text").getAsInt();
+        int FgAtt40_49 = player.getAsJsonObject(STATS).getAsJsonObject(FG_ATT_40_49).get("#text").getAsInt();
+        int Fg40_49Pct = player.getAsJsonObject(STATS).getAsJsonObject(FG_40_49_PCT).get("#text").getAsInt();
+        int FgMade50Plus = player.getAsJsonObject(STATS).getAsJsonObject(FG_MADE_50_PLUS).get("#text").getAsInt();
+        int FgAtt50Plus = player.getAsJsonObject(STATS).getAsJsonObject(FG_ATT_50_PLUS).get("#text").getAsInt();
+        int Fg50PlusPct = player.getAsJsonObject(STATS).getAsJsonObject(FG_50_PLUS_PCT).get("#text").getAsInt();
+        int FgLng = player.getAsJsonObject(STATS).getAsJsonObject(FG_LNG).get("#text").getAsInt();
+
+        int XpBlk = player.getAsJsonObject(STATS).getAsJsonObject(XP_BLK).get("#text").getAsInt();
+        int XpMade = player.getAsJsonObject(STATS).getAsJsonObject(XP_MADE).get("#text").getAsInt();
+        int XpAtt = player.getAsJsonObject(STATS).getAsJsonObject(XP_ATT).get("#text").getAsInt();
+        int XpPct = player.getAsJsonObject(STATS).getAsJsonObject(XP_PCT).get("#text").getAsInt();
+
+        int TwoPtAtt = player.getAsJsonObject(STATS).getAsJsonObject(TWO_PT_ATT).get("#text").getAsInt();
+        int TwoPtMade = player.getAsJsonObject(STATS).getAsJsonObject(TWO_PT_MADE).get("#text").getAsInt();
+        int TwoPtPassAtt = player.getAsJsonObject(STATS).getAsJsonObject(TWO_PT_PASS_ATT).get("#text").getAsInt();
+        int TwoPtPassMade = player.getAsJsonObject(STATS).getAsJsonObject(TWO_PT_PASS_MADE).get("#text").getAsInt();
+        int TwoPtPassRec = player.getAsJsonObject(STATS).getAsJsonObject(TWO_PT_PASS_REC).get("#text").getAsInt();
+        int TwoPtRushAtt = player.getAsJsonObject(STATS).getAsJsonObject(TWO_PT_RUSH_ATT).get("#text").getAsInt();
+        int TwoPtRushMade = player.getAsJsonObject(STATS).getAsJsonObject(TWO_PT_RUSH_MADE).get("#text").getAsInt();
+
         System.out.println(GP);
 
 
         //System.out.println(player.getAsJsonObject(STATS));
-        if(playerPosition.equals(LONGSNAPPER)){
+        if(playerPosition.equals(LONG_SNAPPER)){
             player.getAsJsonObject(PLAYER).remove(ID);
             player.getAsJsonObject(TEAM).remove(ID);
             playerJson.add("player", player.getAsJsonObject(PLAYER));
@@ -219,7 +367,7 @@ public class NFLCpsCollector implements CollectorGeneric {
 
         } else if(playerPosition.equals(KICKER)){
 
-        } else if(playerPosition.equals(RUNNINGBACK) || playerPosition.equals(WIDE_RECEIVER) || playerPosition.equals(TIGHTEND) || playerPosition.equals(FULLBACK)){
+        } else if(playerPosition.equals(RUNNING_BACK) || playerPosition.equals(WIDE_RECEIVER) || playerPosition.equals(TIGHT_END) || playerPosition.equals(FULLBACK)){
 
         } else {
 
